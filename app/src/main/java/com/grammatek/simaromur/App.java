22b4b0ccee6208a655ddd073516cf5243d38939a
Daniel Schnell <dschnell@grammatek.com>
2021-03-02 00:05:32 +0000

    First working version of https://gitlab.com/grammatek/sim/3rdparty/Flite-TTS-Engine-for-Android.
    - packages & names renamed to com.grammatek.simaromur
    - upgraded to use current Android directory structure + settings
    - use CMake as build system instead of ndk-build
    - adapted data file handling to new Android API
    - use original file paths, because this is important for the C/C++ JNI glue
    - still uses hard coded paths for finding static flite libraries
      (see CMake variable FLITE_DIR)
    
    Functionality:
    
    - Voices can be downloded from original URL and are persisted on device
    - Voices can be chosen and are played
    - Integrated into Android TTS settings
    - Quality of voices is quite a bit on the robot side :)
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

---
+++ app/src/main/java/com/grammatek/simaromur/App.java
@@ -0,0 +1,46 @@
+package com.grammatek.simaromur;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
+public class App extends Application {
+
+    private static Application sApplication;
+
+    public static Application getApplication() {
+        return sApplication;
+    }
+
+    public static Context getContext() {
+        return getApplication().getApplicationContext();
+    }
+
+    public static String getAbsoluteFilePath(String relativePath) {
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            return new File(getContext().getExternalFilesDir(null), relativePath).getPath();
+        } else {
+            return new File(getContext().getFilesDir(), relativePath).getPath();
+        }
+    }
+    public static String getDataPath() {
+        return getAbsoluteDirPath("flite-data")+"/";
+    }
+
+
+    public static String getAbsoluteDirPath(String relativePath) {
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            return new File(getContext().getExternalFilesDir(null), relativePath).getPath();
+        } else {
+            return new File(getContext().getFilesDir(), relativePath).getParent();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sApplication = this;
+    }
+}
