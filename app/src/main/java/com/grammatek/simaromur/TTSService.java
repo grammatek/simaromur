800fc7aee9d8061787c4df4bfd80adb92f1c7456
Daniel Schnell <dschnell@grammatek.com>
2021-06-16 18:55:10 +0000

    TTSService.java: remove copyright, we have completely rewritten the class.
    Also some small adaptions because of the renamed class.
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

--- app/src/main/java/com/grammatek/simaromur/TTSService.java
+++ app/src/main/java/com/grammatek/simaromur/TTSService.java
@@ -1,48 +1,8 @@
-/*************************************************************************/
-/*                                                                       */
-/*                  Language Technologies Institute                      */
-/*                     Carnegie Mellon University                        */
-/*                         Copyright (c) 2010                            */
-/*                        All Rights Reserved.                           */
-/*                                                                       */
-/*  Permission is hereby granted, free of charge, to use and distribute  */
-/*  this software and its documentation without restriction, including   */
-/*  without limitation the rights to use, copy, modify, merge, publish,  */
-/*  distribute, sublicense, and/or sell copies of this work, and to      */
-/*  permit persons to whom this work is furnished to do so, subject to   */
-/*  the following conditions:                                            */
-/*   1. The code must retain the above copyright notice, this list of    */
-/*      conditions and the following disclaimer.                         */
-/*   2. Any modifications must be clearly marked as such.                */
-/*   3. Original authors' names are not deleted.                         */
-/*   4. The authors' names are not used to endorse or promote products   */
-/*      derived from this software without specific prior written        */
-/*      permission.                                                      */
-/*                                                                       */
-/*  CARNEGIE MELLON UNIVERSITY AND THE CONTRIBUTORS TO THIS WORK         */
-/*  DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING      */
-/*  ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT   */
-/*  SHALL CARNEGIE MELLON UNIVERSITY NOR THE CONTRIBUTORS BE LIABLE      */
-/*  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    */
-/*  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN   */
-/*  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,          */
-/*  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF       */
-/*  THIS SOFTWARE.                                                       */
-/*                                                                       */
-/*************************************************************************/
-/*             Author:  Alok Parlikar (aup@cs.cmu.edu)                   */
-/*               Date:  June 2012                                        */
-/*************************************************************************/
-
 package com.grammatek.simaromur;
 
-import com.grammatek.simaromur.NativeFliteTTS.SynthReadyCallback;
 import com.grammatek.simaromur.frontend.FrontendManager;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.media.AudioFormat;
 import android.speech.tts.SynthesisCallback;
 import android.speech.tts.SynthesisRequest;
 import android.speech.tts.TextToSpeech;
@@ -53,14 +13,13 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 /**
- * Implements the Flite Engine as a TextToSpeechService
+ * Implements the SIM Engine as a TextToSpeechService
  */
-public class FliteTtsService extends TextToSpeechService {
-    private final static String LOG_TAG = "Simaromur_Java_" + FliteTtsService.class.getSimpleName();
+public class TTSService extends TextToSpeechService {
+    private final static String LOG_TAG = "Simaromur_Java_" + TTSService.class.getSimpleName();
     private FrontendManager mFrontendManager;
     private AppRepository mRepository;
 
