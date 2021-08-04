d64a5fa488ec28188ed803df852c6cbbb52a3b60
Anna Nikulásdóttir <anna@grammatek.com>
2021-08-04 13:54:55 +0000

    api update FrontendManager, class stub syllabstress

--- app/src/main/java/com/grammatek/simaromur/frontend/FrontendManager.java
+++ app/src/main/java/com/grammatek/simaromur/frontend/FrontendManager.java
@@ -24,27 +24,63 @@ public class FrontendManager {
 
     private NormalizationManager mNormalizationManager;
     private Pronunciation mPronunciation;
+    private SyllableStressAnalyzer mSyllabStress;
 
     private Context mContext;
 
     public FrontendManager(Context context) {
+        //TODO: move these initializations up?
+        // we only want to do this once (init POS-model, init g2p, etc.)
+        // and not for each sentence!
         initializeNormalizationManager(context);
         initializePronunciation(context);
         this.mContext = context;
     }
 
     /**
-     * Processes text for input into a TTS engine. This includes unicode cleaning, tokenizing, and
-     * normalizing the the text, and then to convert it into an X-SAMPA transcription.
+     * Processes text for input into a TTS engine. If no boolean parameters are given, we only
+     * normalize the text.
      *
      * @param text raw input text
-     * @return an X-SAMPA transcription of @text
+     * @return a normalized version of @text
      */
     public String process(String text) {
-        String normalized = mNormalizationManager.process(text);
-        String transcribedText = mPronunciation.transcribe(normalized);
-        Log.i(LOG_TAG, text + " => " + transcribedText);
-        return transcribedText;
+        return process(text, false, false);
+    }
+
+    /**
+     * Processes text for input into a TTS engine. The text is normalized, and if @g2p is true,
+     * it is also transcribed and an X-SAMPA transcription of the normalized text is returned.
+     *
+     * @param text raw input text
+     * @return an X-SAMPA transcription of @text if @g2p is true, normalized text otherwise
+     */
+    public String process(String text, boolean g2p) {
+        return process(text, g2p, false);
+    }
+
+    /**
+     * Processes text for input into a TTS engine. Default processing is normalize the text,
+     * if g2p and syllabStress are true, it is further transcribed to X-SAMPA and labelded with
+     * syllables and stress. If g2p is false, syllabStress is redundant and will not be
+     * checked (we can only label transcribed text with syllables and stress).
+     *
+     * @param text raw input text
+     * @return a processed version of @text: plain normalized if @g2p is false, X-SAMPA transcribed
+     * otherwise, with syllable and stress labeling if @syllabStess is true.
+     */
+    public String process(String text, boolean g2p, boolean syllabStress) {
+        String resultText = text;
+        resultText = mNormalizationManager.process(resultText);
+        if (g2p) {
+            resultText = mPronunciation.transcribe(resultText);
+            // we can't perform syllabStress unless we have a transcribed (g2p) version of text
+            if (syllabStress) {
+                resultText = mSyllabStress.label(resultText);
+            }
+        }
+        Log.i(LOG_TAG, text + " => " + resultText);
+        return resultText;
     }
 
     private void initializePronunciation(Context context) {
---
+++ app/src/main/java/com/grammatek/simaromur/frontend/SyllableStressAnalyzer.java
@@ -0,0 +1,9 @@
+package com.grammatek.simaromur.frontend;
+
+public class SyllableStressAnalyzer {
+
+    public String label(String transcribedText) {
+        //TODO: implement syllabification and labeling of transcribed text
+        return transcribedText;
+    }
+}
