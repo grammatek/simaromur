e3fc62c4a5630452acbdf038b7549cd302679e4c
Daniel Schnell <dschnell@grammatek.com>
2021-08-20 17:16:59 +0000

    Set splash screen and show logo. Change accent color to white.
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

--- app/src/main/java/com/grammatek/simaromur/TTSManager.java
+++ app/src/main/java/com/grammatek/simaromur/TTSManager.java
@@ -35,6 +35,7 @@ public class TTSManager extends Activity implements OnItemClickListener, TextToS
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flitemanager);
 
