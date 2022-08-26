434276c242801b56b9a79f047bd584462556da3f
Anna Nikulásdóttir <anna@grammatek.com>
2022-08-26 15:09:26 +0000

    conversion between alphabets added

---
+++ app/src/androidTest/java/com/grammatek/simaromur/G2PTest.java
@@ -0,0 +1,35 @@
+package com.grammatek.simaromur;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.grammatek.simaromur.frontend.Pronunciation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class G2PTest {
+
+    private final static Context context = ApplicationProvider.getApplicationContext();
+
+    @Test
+    public void transcribeTest() {
+        Pronunciation transcriber = new Pronunciation(context);
+        String transcribed = transcriber.transcribe("Hljóðrita þetta");
+        assertEquals("l_0 j ou D r I t a T E h t a", transcribed);
+    }
+
+    @Test
+    public void convertTest() {
+        Pronunciation transcriber = new Pronunciation(context);
+        String converted = transcriber.convert("l_0 j ou: D r I t_h a T E h t a",
+                "SAMPA", "FLITE");
+        assertEquals("lz j ou: D r I th a T E h t a", converted);
+    }
+}
+
--- app/src/main/java/com/grammatek/simaromur/frontend/Pronunciation.java
+++ app/src/main/java/com/grammatek/simaromur/frontend/Pronunciation.java
@@ -10,7 +10,10 @@ import com.grammatek.simaromur.device.SymbolsLvLIs;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,11 +24,13 @@ public class Pronunciation {
     private final Context mContext;
     private NativeG2P mG2P;
     private Map<String, PronDictEntry> mPronDict;
+    private Map<String, Map<String, Map<String, String>>> mAlphabets;
     private String mSilToken = "<sil>";
 
     public Pronunciation(Context context) {
         this.mContext = context;
         initializePronDict();
+        mAlphabets = initializeAlphabets();
     }
 
     public String transcribe(String text) {
@@ -46,6 +51,39 @@ public class Pronunciation {
         return sb.toString().trim();
     }
 
+    public String convert(String transcribed, String fromAlphabet, String toAlphabet) {
+        List<String> validAlpha = getValidAlphabets();
+        if (!validAlpha.contains(fromAlphabet) || !validAlpha.contains(toAlphabet)) {
+            // TODO: handle error
+            System.out.println(fromAlphabet + " and/or " + toAlphabet + " is not a valid" +
+                    " phonetic alphabet. Valid alphabets: " + validAlpha.toString());
+        }
+        if (fromAlphabet.equals(toAlphabet))
+            return transcribed;
+
+        StringBuilder converted = new StringBuilder();
+        Map<String, Map<String, String>> currentDict = mAlphabets.get(fromAlphabet);
+        for (String symbol : transcribed.split(" ")) {
+            if (!currentDict.containsKey(symbol)) {
+                // TODO: log properly
+                System.out.println(symbol + " seems not to be a valid symbol in " + fromAlphabet +
+                        ". Skipping conversion.");
+                converted.append(symbol);
+                converted.append(" ");
+            }
+            else {
+                String convertedSymbol = currentDict.get(symbol).get(toAlphabet);
+                converted.append(convertedSymbol);
+                converted.append(" ");
+            }
+        }
+        return converted.toString().trim();
+    }
+
+    private List<String> getValidAlphabets() {
+        return List.copyOf(mAlphabets.keySet());
+    }
+
     private void initializeG2P() {
         if (mG2P == null) {
             mG2P = new NativeG2P(this.mContext);
@@ -56,6 +94,29 @@ public class Pronunciation {
         if (mPronDict == null) {
             mPronDict = readPronDict();
         }
+    }
+
+    private Map<String, Map<String, Map<String, String>>> initializeAlphabets() {
+        Map<String, Map<String, Map<String, String>>> alphabets = new HashMap<>();
+        List<String> fileContent = readAlphabets();
+        List<String> headers = Arrays.asList(fileContent.get(0).split("\t"));
+        for (String header : headers) {
+            int ind = headers.indexOf(header);
+            alphabets.put(header, new HashMap());
+            for (int i = 1; i < fileContent.size(); i++) {
+                List<String> symbols = Arrays.asList(fileContent.get(i).split("\t"));
+                String keySymbol = symbols.get(ind);
+                Map<String, String> symbolDict = new HashMap<>();
+                for (String h : headers) {
+                    if (headers.indexOf(h) == ind)
+                        continue;
+                    else
+                        symbolDict.put(h, symbols.get(headers.indexOf(h)));
+                }
+                alphabets.get(header).put(keySymbol, symbolDict);
+            }
+        }
+        return alphabets;
     }
 
     private Map<String, PronDictEntry> readPronDict() {
@@ -78,5 +139,24 @@ public class Pronunciation {
             e.printStackTrace();
         }
         return pronDict;
+    }
+
+    private List<String> readAlphabets() {
+        Resources res = this.mContext.getResources();
+        int resID = R.raw.sampa_ipa_single_flite;
+        List<String> fileContent = new ArrayList<>();
+        String line = "";
+        try {
+            InputStream is = res.openRawResource(resID);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            if (is != null) {
+                while ((line = reader.readLine()) != null) {
+                    fileContent.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 }
